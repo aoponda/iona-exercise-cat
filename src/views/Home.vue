a3e1a282c91c8cd8deeb07c60794a5512d07824e
Albert Oponda <albertneil.oponda@gmail.com>
2022-12-26 04:25:36 +0800

    Implementation of Load More functionality in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,7 +18,7 @@
     <b-container class="py-3" v-show="selectedBreed">
       <b-card-group columns>
         <b-card
-          v-for="breedImage in breedImages"
+          v-for="breedImage in loadMoreBreedImages"
           :key="breedImage.id"
           :title="breedImage.name"
           :img-src="breedImage.url"
@@ -40,6 +40,15 @@
           </template>
         </b-card>
       </b-card-group>
+
+      <p class="p-flat text-center py-5">
+        <b-button
+          v-if="imagesVisible < breedImages.length"
+          @click="imagesVisible += increment"
+          variant="primary"
+          >Load more Images</b-button
+        >
+      </p>
     </b-container>
   </b-container>
 </template>
@@ -59,13 +68,18 @@ export default defineComponent({
       isBreed: true,
       breedList: [] as Breed[],
       breedImages: [] as Image[],
+      displayedBreedImages: [] as Image[],
       selectedBreed: this.$route.query.breed
         ? String(this.$route.query.breed)
         : "",
+      imagesVisible: 6,
+      increment: 3,
     };
   },
   watch: {
     selectedBreed: function () {
+      this.imagesVisible = 6;
+      this.displayedBreedImages = [];
       this.getBreedImages();
     },
   },
@@ -89,6 +103,10 @@ export default defineComponent({
       ImageDataService.getBreedImages(this.selectedBreed)
         .then((response) => {
           this.breedImages = response.data;
+          this.displayedBreedImages = this.breedImages.slice(
+            0,
+            this.imagesVisible
+          );
           console.log(response.data);
         })
         .catch((e: Error) => {
@@ -101,6 +119,11 @@ export default defineComponent({
       } else {
         // Do nothing
       }
+    },
+  },
+  computed: {
+    loadMoreBreedImages() {
+      return this.breedImages.slice(0, this.imagesVisible);
     },
   },
   mounted() {
